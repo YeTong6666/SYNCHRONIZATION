77764ba6f86668b19f8886b1480e895afd3b4e8e
YeTong6666 <1501045805@qq.com>
2022-02-15 10:42:31 +0800

    main

---
+++ C/love.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int love=0 ,end;
+    while you.say("see u tomorrow") do       //你的一句明天见，偷走了我整晚的睡眠
+        I.sleep=0
+    end
+
+    while(love<end){          
+        love++;
+    }//直到生命结束那一刻，每天爱你多一点
+        
+}
